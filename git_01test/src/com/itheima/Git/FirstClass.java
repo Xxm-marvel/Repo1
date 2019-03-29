8e3608eb216c1651ed8fa0ef9b563c9ad12b9a1f
admin <yoei984245@163.com>
2019-03-29 20:22:56 +0800

    本地文件提交

---
+++ git_01test/src/com/itheima/Git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima.Git;
+
+/**
+ * @author 许晓明
+ * @date 2019/3/29 20:17
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hei,world");
+        System.out.println("hei,world");
+        System.out.println("hei,world");
+        System.out.println("hei,world");
+        System.out.println("hei,world");
+        System.out.println("hei,world");
+    }
+}
